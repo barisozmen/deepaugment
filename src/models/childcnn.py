a5e44a8db196261fe328138bc990904c509ea410
Baris Ozmen <hbaristr@gmail.com>
2019-01-30 01:50:58 -0800

    code formatted by black

--- src/models/childcnn.py
+++ src/models/childcnn.py
@@ -16,32 +16,30 @@ class ChildCNN:
 
     def create_child_cnn(self):
         model = Sequential()
-        model.add(Conv2D(32, (3, 3), padding='same',input_shape=self.input_shape))
-        model.add(Activation('relu'))
+        model.add(Conv2D(32, (3, 3), padding="same", input_shape=self.input_shape))
+        model.add(Activation("relu"))
         model.add(Conv2D(32, (3, 3)))
-        model.add(Activation('relu'))
+        model.add(Activation("relu"))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
-        model.add(Conv2D(64, (3, 3), padding='same'))
-        model.add(Activation('relu'))
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("relu"))
         model.add(Conv2D(64, (3, 3)))
-        model.add(Activation('relu'))
+        model.add(Activation("relu"))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
         model.add(Flatten())
         model.add(Dense(512))
-        model.add(Activation('relu'))
+        model.add(Activation("relu"))
         model.add(Dropout(0.5))
         model.add(Dense(self.num_classes))
-        model.add(Activation('softmax'))
+        model.add(Activation("softmax"))
 
         optimizer = optimizers.RMSprop(lr=0.0001, decay=1e-6)
-        #optimizer = optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0)
+        # optimizer = optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0)
         model.compile(
-            optimizer=optimizer,
-            loss="categorical_crossentropy",
-            metrics=["accuracy"]
+            optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"]
         )
         return model
