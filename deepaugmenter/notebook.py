2e69ec379a636fabe4b6e98aa75879b316e090a1
Baris Ozmen <hbaristr@gmail.com>
2019-02-03 12:02:05 -0800

    refactoring: simplified run_bayesianopt()
    Former-commit-id: b8468e673df92f513eaa489e8d8084c306c91b32

--- deepaugmenter/notebook.py
+++ deepaugmenter/notebook.py
@@ -8,7 +8,7 @@ class Notebook():
         self.df = pd.DataFrame()
         self.store_path = store_path
 
-    def record(self, trial_no, trial_hyperparams, sample_no, cost, history):
+    def record(self, trial_no, trial_hyperparams, sample_no, mean_late_val_acc, history):
         new_df = pd.DataFrame(history)
         new_df["trial_no"] = trial_no
         new_df["aug1_type"] = trial_hyperparams[0]
@@ -17,7 +17,7 @@ class Notebook():
         new_df["aug2_magnitude"] = trial_hyperparams[3]
         new_df["portion"] = trial_hyperparams[4]
         new_df["sample_no"] = sample_no
-        new_df["mean_late_val_acc"] = cost
+        new_df["mean_late_val_acc"] = mean_late_val_acc
         new_df.round(3) # round all float values to 3 decimals after point
         self.df = pd.concat([self.df, new_df])
 
