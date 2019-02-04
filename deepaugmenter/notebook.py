9700d286b641a0fe74c2162f63eb77d898ba8717
Baris Ozmen <hbaristr@gmail.com>
2019-02-03 22:40:02 -0800

    notebook.py refactoring
    Former-commit-id: 0607904ea3318f037a17060e961e8f0b2192db72

--- deepaugmenter/notebook.py
+++ deepaugmenter/notebook.py
@@ -1,6 +1,7 @@
 # (C) 2019 Baris Ozmen <hbaristr@gmail.com>
 
 import pandas as pd
+import numpy as np
 
 class Notebook():
 
@@ -8,7 +9,7 @@ class Notebook():
         self.df = pd.DataFrame()
         self.store_path = store_path
 
-    def record(self, trial_no, trial_hyperparams, sample_no, mean_late_val_acc, history):
+    def record(self, trial_no, trial_hyperparams, sample_no, reward, history):
         new_df = pd.DataFrame(history)
         new_df["trial_no"] = trial_no
         new_df["aug1_type"] = trial_hyperparams[0]
@@ -19,8 +20,9 @@ class Notebook():
         new_df["aug3_magnitude"] = trial_hyperparams[5]
         new_df["portion"] = trial_hyperparams[6]
         new_df["sample_no"] = sample_no
-        new_df["mean_late_val_acc"] = mean_late_val_acc
+        new_df["mean_late_val_acc"] = reward
         new_df = new_df.round(3) # round all float values to 3 decimals after point
+        new_df["epoch"] = np.arange(1, len(new_df)+1)
         self.df = pd.concat([self.df, new_df])
 
     def save(self):
