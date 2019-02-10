2338dd18cf7c4c98eaca24ac50b82dd4c25e7dce
Baris Ozmen <hbaristr@gmail.com>
2019-02-09 18:50:25 -0800

    code formatted by black

--- deepaugment/image_generator.py
+++ deepaugment/image_generator.py
@@ -1,4 +1,3 @@
-
 import keras
 
 import numpy as np
@@ -9,13 +8,14 @@ from lib.cutout import cutout_numpy
 
 
 def random_flip(x):
-  """Flip the input x horizontally with 50% probability."""
-  if np.random.rand(1)[0] > 0.5:
-    return np.fliplr(x)
-  return x
+    """Flip the input x horizontally with 50% probability."""
+    if np.random.rand(1)[0] > 0.5:
+        return np.fliplr(x)
+    return x
+
 
 def zero_pad_and_crop(img, amount=4):
-  """Zero pad by `amount` zero pixels on each side then take a random crop.
+    """Zero pad by `amount` zero pixels on each side then take a random crop.
   Args:
     img: numpy image that will be zero padded and cropped.
     amount: amount of zeros to pad `img` with horizontally and verically.
@@ -23,15 +23,14 @@ def zero_pad_and_crop(img, amount=4):
     The cropped zero padded img. The returned numpy array will be of the same
     shape as `img`.
   """
-  padded_img = np.zeros((img.shape[0] + amount * 2, img.shape[1] + amount * 2,
-                         img.shape[2]))
-  padded_img[amount:img.shape[0] + amount, amount:
-             img.shape[1] + amount, :] = img
-  top = np.random.randint(low=0, high=2 * amount)
-  left = np.random.randint(low=0, high=2 * amount)
-  new_img = padded_img[top:top + img.shape[0], left:left + img.shape[1], :]
-  return new_img
-
+    padded_img = np.zeros(
+        (img.shape[0] + amount * 2, img.shape[1] + amount * 2, img.shape[2])
+    )
+    padded_img[amount : img.shape[0] + amount, amount : img.shape[1] + amount, :] = img
+    top = np.random.randint(low=0, high=2 * amount)
+    left = np.random.randint(low=0, high=2 * amount)
+    new_img = padded_img[top : top + img.shape[0], left : left + img.shape[1], :]
+    return new_img
 
 
 def apply_default_transformations(X):
@@ -57,9 +56,11 @@ def deepaugment_image_generator(X, y, policy, batch_size=64, augment_chance=0.5)
 
     Returns:
     """
-    if type(policy)==str:
+    if type(policy) == str:
         policy_df = pd.read_csv(policy)
-        policy_df = policy_df[["aug1_type","aug1_magnitude","aug2_type","aug2_magnitude","portion"]]
+        policy_df = policy_df[
+            ["aug1_type", "aug1_magnitude", "aug2_type", "aug2_magnitude", "portion"]
+        ]
         policy = policy_df.to_dict(orient="records")
 
     print("Policies are:")
@@ -72,24 +73,28 @@ def deepaugment_image_generator(X, y, policy, batch_size=64, augment_chance=0.5)
         ix = np.arange(len(X))
         np.random.shuffle(ix)
         for i in range(len(X) // batch_size):
-            _ix = ix[i * batch_size:(i + 1) * batch_size]
+            _ix = ix[i * batch_size : (i + 1) * batch_size]
             _X = X[_ix]
             _y = y[_ix]
 
-            tiny_batch_size=4
+            tiny_batch_size = 4
             aug_X = _X[0:tiny_batch_size]
             aug_y = _y[0:tiny_batch_size]
             for j in range(1, len(_X) // tiny_batch_size):
-                tiny_X = _X[j * tiny_batch_size:(j + 1) * tiny_batch_size]
-                tiny_y = _y[j * tiny_batch_size:(j + 1) * tiny_batch_size]
+                tiny_X = _X[j * tiny_batch_size : (j + 1) * tiny_batch_size]
+                tiny_y = _y[j * tiny_batch_size : (j + 1) * tiny_batch_size]
                 if np.random.rand() <= augment_chance:
                     aug_chain = np.random.choice(policy)
-                    aug_chain['portion'] = 1.0 # last element is portion, which we want to be 1
+                    aug_chain[
+                        "portion"
+                    ] = 1.0  # last element is portion, which we want to be 1
                     hyperparams = list(aug_chain.values())
 
                     aug_data = augmenter.run(tiny_X, tiny_y, *hyperparams)
 
-                    aug_data["X_train"] = apply_default_transformations(aug_data["X_train"])
+                    aug_data["X_train"] = apply_default_transformations(
+                        aug_data["X_train"]
+                    )
 
                     aug_X = np.concatenate([aug_X, aug_data["X_train"]])
                     aug_y = np.concatenate([aug_y, aug_data["y_train"]])
@@ -105,7 +110,7 @@ def test_deepaugment_image_generator():
     y = np.random.randint(10, size=200)
     y = keras.utils.to_categorical(y)
 
-    batch_size=64
+    batch_size = 64
 
     policy = [
         {
@@ -125,11 +130,10 @@ def test_deepaugment_image_generator():
             "aug3_type": "clouds",
             "aug3_magnitude": 0.2,
             "portion": 0.2,
-        }
+        },
     ]
 
-    return deepaugment_generator(X,y,policy,batch_size=batch_size)
-
+    return deepaugment_generator(X, y, policy, batch_size=batch_size)
 
 
 if __name__ == "__main__":
--- deepaugment/run_full_model.py
+++ deepaugment/run_full_model.py
@@ -7,15 +7,20 @@ import datetime
 
 import sys
 from os.path import dirname, realpath
+
 file_path = realpath(__file__)
 dir_of_file = dirname(file_path)
 parent_dir_of_file = dirname(dir_of_file)
 sys.path.insert(0, parent_dir_of_file)
 
 now = datetime.datetime.now()
-EXPERIMENT_NAME = f"wrn_28_10_{now.year}-{now.month:02}-{now.day:02}_{now.hour:02}-{now.minute:02}"
+EXPERIMENT_NAME = (
+    f"wrn_28_10_{now.year}-{now.month:02}-{now.day:02}_{now.hour:02}-{now.minute:02}"
+)
 
-EXPERIMENT_FOLDER_PATH = os.path.join(parent_dir_of_file, f"reports/experiments/{EXPERIMENT_NAME}")
+EXPERIMENT_FOLDER_PATH = os.path.join(
+    parent_dir_of_file, f"reports/experiments/{EXPERIMENT_NAME}"
+)
 log_path = pathlib.Path(EXPERIMENT_FOLDER_PATH)
 log_path.mkdir(parents=True, exist_ok=True)
 logging.basicConfig(filename=(log_path / "info.log").absolute(), level=logging.DEBUG)
@@ -25,6 +30,7 @@ from augmenter import Augmenter
 from childcnn import ChildCNN
 from build_features import DataOp
 from lib.decorators import Reporter
+
 logger = Reporter.logger
 
 from keras.callbacks import CSVLogger
@@ -32,6 +38,8 @@ from keras.callbacks import CSVLogger
 from image_generator import deepaugment_image_generator
 
 import click
+
+
 @click.command()
 @click.option("--dataset-name", type=click.STRING, default="cifar10")
 @click.option("--num-classes", type=click.INT, default=10)
@@ -45,17 +53,21 @@ def run_model(dataset_name, num_classes, epochs, batch_size, policies_path):
     data = DataOp.preprocess_normal(data)
 
     wrn_28_10 = ChildCNN(
-        model_name="wrn_28_10", input_shape=input_shape,
-        batch_size=batch_size, num_classes=num_classes,
+        model_name="wrn_28_10",
+        input_shape=input_shape,
+        batch_size=batch_size,
+        num_classes=num_classes,
         pre_augmentation_weights_path="initial_model_weights.h5",
-        logging=logging
+        logging=logging,
     )
 
     if policies_path == "dont_augment":
         policy_str = "non_augmented"
     else:
         policy_str = "augmented"
-    csv_logger = CSVLogger(f"{EXPERIMENT_FOLDER_PATH}/wrn_28_10_training_on_{dataset_name}_{policy_str}.csv")
+    csv_logger = CSVLogger(
+        f"{EXPERIMENT_FOLDER_PATH}/wrn_28_10_training_on_{dataset_name}_{policy_str}.csv"
+    )
 
     if policies_path == "dont_augment":
         history = wrn_28_10.fit_normal(data, epochs=epochs, csv_logger=csv_logger)
@@ -63,15 +75,20 @@ def run_model(dataset_name, num_classes, epochs, batch_size, policies_path):
     else:
 
         datagen = deepaugment_image_generator(
-            data["X_train"], data["y_train"],
-            policies_path, batch_size=batch_size,
-            augment_chance=0.8
+            data["X_train"],
+            data["y_train"],
+            policies_path,
+            batch_size=batch_size,
+            augment_chance=0.8,
         )
         print("fitting the model")
         history = wrn_28_10.fit_with_generator(
-            datagen, data["X_val"], data["y_val"],
-            train_data_size = len(data["X_train"]),
-            epochs=epochs, csv_logger=csv_logger
+            datagen,
+            data["X_val"],
+            data["y_val"],
+            train_data_size=len(data["X_train"]),
+            epochs=epochs,
+            csv_logger=csv_logger,
         )
         print(f"Reached validation accuracy is {history['val_acc'][-1]}")
 
