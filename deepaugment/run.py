adb617387d0777f90459cfa1871b682e3ffd9251
Baris Ozmen <hbaristr@gmail.com>
2019-02-11 11:17:07 -0800

    refactoring

--- deepaugment/run.py
+++ deepaugment/run.py
@@ -9,9 +9,11 @@ from deepaugment import DeepAugment
 
 deepaug = DeepAugment("cifar10", config={"child_epochs":1})
 
-# best_policies = deepaug.optimize(1)
+best_policies = deepaug.optimize(1)
 
-# best_policies_2 = deepaug.optimize(1)
+best_policies_2 = deepaug.optimize(1)
+
+
 
 print ("best-policies:", best_policies)
 print ("best-policies-2:", best_policies_2)
