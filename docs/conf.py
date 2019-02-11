dede6456fcb4005364ef07d86b8abc1b1869d99f
Baris Ozmen <hbaristr@gmail.com>
2019-02-11 01:03:05 -0800

    some libs mocked in docs
    Former-commit-id: 252a3ff0ceaca404df2d140162ceed5dae2e0ea3

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,17 @@ sys.path.insert(0, "../deepaugment")
 sys.path.insert(0, "../deepaugment/lib")
 
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['keras', 'tensorflow', 'pandas', 'argparse', 'numpy', 'pandas', 'click']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # -- Project information -----------------------------------------------------
 
 project = 'deepaugment'
