5d094c080fc0835e077724717a30b0c96078e3c0
Baris Ozmen <hbaristr@gmail.com>
2019-02-13 19:41:05 -0800

    identation fixed

--- deepaugment/build_features.py
+++ deepaugment/build_features.py
@@ -109,8 +109,7 @@ class DataOp:
 
     @staticmethod
     def sample_validation_set(data):
-	val_seed_size = len(data["X_val_seed"])
-
+    val_seed_size = len(data["X_val_seed"])
         ix = np.random.choice(range(len(val_seed_size), min(val_seed_size, 1000), False)
         X_val = data["X_val_seed"][ix].copy()
         y_val = data["y_val_seed"][ix].copy()
