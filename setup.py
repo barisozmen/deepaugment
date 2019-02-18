7c0cee7ae28ad75c1d1af532096c0a10236f6a06
Baris Ozmen <hbaristr@gmail.com>
2019-02-17 19:34:24 -0800

    version increased
    Former-commit-id: 966cb47cb8adc24c4e3da422b44245fbee1429f9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = "0.8.0"
+__version__ = "0.12.1"
 
 here = path.abspath(path.dirname(__file__))
 
