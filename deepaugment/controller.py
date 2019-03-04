25d7c11be895f8edc4f9a75b2a29b4843f0d2baa
Baris Ozmen <hbaristr@gmail.com>
2019-03-03 21:50:22 -0800

    now deepaugment optimizes for 5-policies

--- deepaugment/controller.py
+++ deepaugment/controller.py
@@ -68,11 +68,30 @@ class Controller:
 
         self.opt = skopt.Optimizer(
             [
-                skopt.space.Categorical(AUG_TYPES, name="aug1_type"),
-                skopt.space.Real(0.0, 1.0, name="aug1_magnitude"),
-                skopt.space.Categorical(AUG_TYPES, name="aug2_type"),
-                skopt.space.Real(0.0, 1.0, name="aug2_magnitude"),
-                skopt.space.Real(0.0, 1.0, name="portion"),
+                skopt.space.Categorical(AUG_TYPES, name="A_aug1_type"),
+                skopt.space.Real(0.0, 1.0, name="A_aug1_magnitude"),
+                skopt.space.Categorical(AUG_TYPES, name="A_aug2_type"),
+                skopt.space.Real(0.0, 1.0, name="A_aug2_magnitude"),
+
+                skopt.space.Categorical(AUG_TYPES, name="B_aug1_type"),
+                skopt.space.Real(0.0, 1.0, name="B_aug1_magnitude"),
+                skopt.space.Categorical(AUG_TYPES, name="B_aug2_type"),
+                skopt.space.Real(0.0, 1.0, name="B_aug2_magnitude"),
+
+                skopt.space.Categorical(AUG_TYPES, name="C_aug1_type"),
+                skopt.space.Real(0.0, 1.0, name="C_aug1_magnitude"),
+                skopt.space.Categorical(AUG_TYPES, name="C_aug2_type"),
+                skopt.space.Real(0.0, 1.0, name="C_aug2_magnitude"),
+
+                skopt.space.Categorical(AUG_TYPES, name="D_aug1_type"),
+                skopt.space.Real(0.0, 1.0, name="D_aug1_magnitude"),
+                skopt.space.Categorical(AUG_TYPES, name="D_aug2_type"),
+                skopt.space.Real(0.0, 1.0, name="D_aug2_magnitude"),
+
+                skopt.space.Categorical(AUG_TYPES, name="E_aug1_type"),
+                skopt.space.Real(0.0, 1.0, name="E_aug1_magnitude"),
+                skopt.space.Categorical(AUG_TYPES, name="E_aug2_type"),
+                skopt.space.Real(0.0, 1.0, name="E_aug2_magnitude")
             ],
             n_initial_points=opt_initial_points,
             base_estimator="RF",  # Random Forest estimator
